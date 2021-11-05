65403cc3dd17e12b7c02b478773259811a9f1eac
Kamaal <kamaal@tranzer.com>
2021-11-05 17:28:56 +0100

    Giving up testing because of entitlements

---
+++ Tests/ICloutKitTests/ICloutKitTests.swift
@@ -0,0 +1,15 @@
+//
+//  ICloutKitTests.swift
+//  
+//
+//  Created by Kamaal M Farah on 04/11/2021.
+//
+
+import XCTest
+@testable import ICloutKit
+
+final class ICloutKitTests: XCTestCase {
+    func testExample() {
+        XCTAssertEqual(true, true)
+    }
+}
